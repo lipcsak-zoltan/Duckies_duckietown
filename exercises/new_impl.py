a1549c259a0615efd6cbfbd82c388c5e7d9f014b
Matyi <p.matyi00@gmail.com>
2022-12-11 00:25:59 -0800

    correct learning

--- exercises/new_impl.py
+++ exercises/new_impl.py
@@ -1,9 +1,12 @@
-import gym 
-from stable_baselines3 import DQN
-from stable_baselines3 import PPO, HerReplayBuffer
+import gym
+from stable_baselines3 import DQN, HerReplayBuffer
+from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import VecFrameStack
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.logger import configure
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.callbacks import CheckpointCallback
+
 import os
 
 import pyglet
@@ -59,7 +62,7 @@ class SelfEnv(gym.Env):    # The wrapper encapsulates the Duckietown env
         #self.action_space = self.duckie_env.action_space
         self.action_space = gym.spaces.Discrete(3)
         self.observation_space = gym.spaces.Box(
-            low=0, high=255, shape=(40, 80, 4), dtype=np.uint8
+            low=0, high=255, shape=(40, 80, 3), dtype=np.uint8
         )
         self.reward_range = self.duckie_env.reward_range
 
@@ -68,14 +71,14 @@ class SelfEnv(gym.Env):    # The wrapper encapsulates the Duckietown env
         self.full_step_idx = 0
         self.acc_reward = 0
         self.iter_idx = 0
-
+        #self.obs_seq = np.zeros((40, 80, 20))
     def step(self, action):
-        if action == 0:
-            new_action = np.array([0.04, 0.4])
-        elif action == 1:
-            new_action = np.array([0.4, 0.04])
-        elif action == 2:
-            new_action = np.array([0.03, 0.3])
+        if action == 0: # forward
+            new_action = np.array([0.23, 0]) * 2
+        elif action == 1: # right
+            new_action = np.array([0.09, -0.39]) * 2 
+        elif action == 2: # left
+            new_action = np.array([0.12, 0.38]) * 2
         obs, reward, done, info = self.duckie_env.step(new_action)   # calls the gym env methods
         #obs = self._blur(obs)                             # applies your specific treatment
         obs = self.preprocess_obs(obs)
@@ -88,7 +91,8 @@ class SelfEnv(gym.Env):    # The wrapper encapsulates the Duckietown env
         self.full_step_idx += 1
         self.step_idx += 1
 
-        
+        if done:
+            reward = -30
         return obs, reward, done, info
 
     def reset(self, segment=False):
@@ -101,13 +105,13 @@ class SelfEnv(gym.Env):    # The wrapper encapsulates the Duckietown env
 
     def render(self, mode = "human", close = False, segment = False):
         return self.duckie_env.render(mode, close, segment)
-    
+
     def close(self):
         return self.duckie_env.close()
 
     def seed(self, seed=None):
         return self.duckie_env.seed(seed)
-    
+
     def preprocess_obs(self, obs):
         obs = cv2.resize(obs, (80, 60))
         obs = obs[20:,:,:]
@@ -117,9 +121,10 @@ class SelfEnv(gym.Env):    # The wrapper encapsulates the Duckietown env
         red_mask = cv2.inRange(hsv, (116, 35, 0), (179, 255, 255))
         yellow_mask = cv2.inRange(hsv, (21, 61, 151), (75, 255, 255))
         white_mask = cv2.inRange(hsv, (0, 0, 137), (150, 36, 255))
-        black_mask = cv2.inRange(hsv, (0, 0, 0), (179, 120, 95))
+        #black_mask = cv2.inRange(hsv, (0, 0, 0), (179, 120, 95))
 
-        obs = np.stack([red_mask, yellow_mask, white_mask, black_mask], axis=2)
+        obs = np.stack([red_mask, yellow_mask, white_mask], axis=2)
+        #self.obs_seq = np.concatenate((obs, self.obs_seq[:,:,:16]), axis = 2)
         return obs
 
 # class CustomCNN(BaseFeaturesExtractor):
@@ -172,33 +177,40 @@ subdir = time.strftime("%d-%b-%Y_%H.%M.%S", now)
 log_path = os.path.join('Training', 'Logs', subdir)
 orig_log_path = os.path.join('Training', 'Logs')
 
-#new_logger = configure(log_path, ["stdout", "csv", "tensorboard"])
-#env = DuckietownEnv(map_name = "udem1", domain_rand = False, draw_bbox = False)
+
+
 env = SelfEnv(log_path, map_name = "loop_empty", domain_rand = False, draw_bbox = False)
 
+checkpoint_callback = CheckpointCallback(
+  save_freq=5000,
+  save_path=log_path,
+  name_prefix="rl_model"
+)
 
 #obs = env.reset()
 #env.render()
 
+#model = DQN("CnnPolicy", env, buffer_size=50000, verbose=1, tensorboard_log=orig_log_path, exploration_fraction=0.05, learning_rate=0.00005)
 model = PPO("CnnPolicy", env, verbose=1, tensorboard_log=orig_log_path)
+
 #model.set_logger(new_logger)
 
-model.learn(1000000)
+model.learn(1000000, callback=checkpoint_callback)
 
 
-# env = DuckietownEnv(map_name = "zigzag_dists", domain_rand = False, draw_bbox = False) 
+# env = DuckietownEnv(map_name = "zigzag_dists", domain_rand = False, draw_bbox = False)
 # model.set_env(env)
 # model.learn(total_timesteps=100)
 
-# env = DuckietownEnv(map_name = "small_loop_cw", domain_rand = False, draw_bbox = False) 
+# env = DuckietownEnv(map_name = "small_loop_cw", domain_rand = False, draw_bbox = False)
 # model.set_env(env)
 # model.learn(total_timesteps=100)
 
-# env = DuckietownEnv(map_name = "zigzag_dists", domain_rand = False, draw_bbox = False) 
+# env = DuckietownEnv(map_name = "zigzag_dists", domain_rand = False, draw_bbox = False)
 #  model.set_env(env)
 
 model.save("first_model")
 
-#valuate_policy(model, env, n_eval_episodes=10, render=True)
+#evaluate_policy(model, env, n_eval_episodes=10, render=True)
 
 env.close()
